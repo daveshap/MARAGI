6bd95a5079bca42cebfe7fde6132e2bc6eaca059
David Shapiro <daveshap37@gmail.com>
2020-01-06 19:08:47 -0500

    tightening up the server and client

--- maragi/client.py
+++ maragi/client.py
@@ -5,44 +5,56 @@ class Client():
     def __init__(self, ip='127.0.0.1', port='9999'):
         self.ip = ip
         self.port = port
-        self.url = 'http://%s:%s/' % (ip,port)
-        # HTTPS support might come eventually, if there's a need
-        self.api_new = self.url + 'new/'
-        self.api_query = self.url + 'fetch/'
-        self.age = self.url + 'age/'
-        self.range = self.url + 'range/'
+        self.url = 'http://%s:%s' % (ip,port)
+        # TODO HTTPS support might come eventually, if there's a need
     
     
-    def get_from_endpoint(url):
+    def get_from_endpoint(self, url):
         # reusable function to quickly GET from URL
         response = requests.request(method='GET', url=url)
-        data = response.json()
-        data = sorted(data, key = lambda i: i['time'])
-        return data
+        return response.json()
+        
     
-    
-    def send(self, payload):
+    def send(self, service, data, metadata):
         # add a new message to the SOC
+        # TODO check that data is JSON-friendly
+        # TODO check that metadata is a string
+        payload = {'service': service, 'data': data, 'metadata': metadata}
         response = requests.request(method='POST', url=self.api_new, json=payload)
         print(response.status_code, response.text)
         
     
-    def fetch(self, field, keyword):
-        # fetch SOC logs based on keyword in the messages
-        api_endpoint = self.message + '%s/%s' % (field, keyword)
-        data = self.get_from_endpoint(api_endpoint)
-        return data
-        
+    def get_recent_meta(self, seconds, value):
+        # get all message of n seconds old or less with specific value in metadata
+        endpoint = self.url + '/recent/meta/%s/%s' % (seconds, value)
+        return self.get_from_endpoint(endpoint)
     
-    def fetch_recent(self, max_age):
-        # fetch SOC logs based on recency
-        api_endpoint = self.age + str(max_age)
-        data = self.get_from_endpoint(api_endpoint)
-        return data
-        
-        
-    def fetch_range(self, start, end):
+    
+    def get_recent_data(self, seconds, value):
+        # get all message of n seconds old or less with specific value in data
+        endpoint = self.url + '/recent/data/%s/%s' % (seconds, value)
+        return self.get_from_endpoint(endpoint)
+    
+    
+    def get_recent(self, seconds):
+        # get all message of n seconds old or less 
+        endpoint = self.url + '/recent/seconds/%s' % seconds
+        return self.get_from_endpoint(endpoint)
+    
+    
+    def get_all(self):
+        # get all messages
+        endpoint = self.url + '/all'
+        return self.get_from_endpoint(endpoint)
+    
+    
+    def get_uuid(self, uuid):
+        # get all messages
+        endpoint = self.url + '/uuid'
+        return self.get_from_endpoint(endpoint)
+
+    
+    def get_range(self, start, end):
         # fetch SOC logs between two timestamps
-        api_endpoint = self.range + '%s/%s' % (start, end)
-        data = self.get_from_endpoint(api_endpoint)
-        return data
+        endpoint = self.url + '/range/%s/%s' % (start, end)
+        return self.get_from_endpoint(endpoint)
--- maragi/server.py
+++ maragi/server.py
@@ -6,11 +6,13 @@ from uuid import uuid4
 
 
 class Server():
-    def __init__(self, port=9999, fields=[], soc_file='soc.json'):
+    def __init__(self, port=9999):
         self.port = port
-        self.soc_file = soc_file
-        self.fields = fields + ['time', 'uuid', 'service']
+        self.fields = ['time', 'uuid', 'service', 'data', 'metadata']
         self.soc_log = []
+        # TODO save to file
+        # TODO ship to syslog
+        # TODO ship to AMQP
 
 
     def validate_message(self, message):
@@ -35,6 +37,17 @@ class Server():
         print('SOC log loaded')
 
 
+    def filter_age(self, seconds):
+        max_age = time() - int(seconds)
+        result = [i for i in self.soc_log if i['time']>=max_age]
+        return result       
+
+
+    def filter_field(self, field, value):
+        result = [i for i in self.soc_log if value.lower() in i[field].lower()]
+        return result
+
+
     def run(self):
         app = flask.Flask('maragi')
         
@@ -42,32 +55,38 @@ class Server():
         # register a new message with the SOC
         def new_message():
             payload = request.json
-            payload['time'] = str(time())
+            payload['time'] = time()
             payload['uuid'] = str(uuid4())
             if self.validate_message(payload):
                 self.soc_log.append(payload)
-                with open(self.soc_file, 'w') as outfile:
-                    json.dump(self.soc_log, outfile, separators=(',\n ', ': '))
+                print(payload)
                 return 'Log accepted and saved', 200
             else:
                 return 'Log rejected, missing field', 402
 
-        @app.route('/fetch/<field>/<keyword>', methods=['GET'])
-        # return all messages from SOC where a particular term is in a specified field
-        def fetch_field(field, keyword):
-            result = [i for i in self.soc_log if keyword.lower() in i[field].lower()]
+        @app.route('/recent/meta/<seconds>/<value>', methods=['GET'])
+        # fetch most recent messages of age n seconds or less that match a certain field in the metadata
+        def recent_meta(seconds,value):
+            recent = filter_age(seconds)
+            result = filter_field('metadata', value)
             return flask.Response(json.dumps(result), mimetype='application/json')
-
-        @app.route('/age/<seconds>', methods=['GET'])
-        # return SOC entries that are N seconds old or newer
-        def fetch_logs_age(seconds):
-            max_age = time() - int(seconds)
-            payload = [i for i in self.soc_log if i['time']>max_age]
-            return flask.Response(json.dumps(payload), mimetype='application/json')
-           
+        
+        @app.route('/recent/data/<seconds>/<value>', methods=['GET'])
+        # fetch most recent messages of age n seconds or less that match a certain field in the data
+        def recent_data(seconds,value):
+            recent = filter_age(seconds)
+            result = filter_field('data', value)
+            return flask.Response(json.dumps(result), mimetype='application/json')
+        
+        @app.route('/recent/seconds/<seconds>', methods=['GET'])
+        # fetch all messages within certain age
+        def recent_seconds(seconds):
+            recent = filter_age(seconds)
+            return flask.Response(json.dumps(recent), mimetype='application/json')
+        
         @app.route('/range/<start>/<end>', methods=['GET'])
         # return SOC entries between two specified unix epoch timestamps
-        def fetch_logs_range(start, end):
+        def fetch_range(start, end):
             payload = [i for i in self.soc_log if i['time']>=int(start) and i['time']<=int(end)]
             return flask.Response(json.dumps(payload), mimetype='application/json')
 
@@ -76,9 +95,11 @@ class Server():
         def fetch_all():
             return flask.Response(json.dumps(self.soc_log), mimetype='application/json')
             
-        @app.route('/fields', methods=['GET'])
-        # return list of fields current SOC is using
-            return flask.Response(json.dumps(self.fields), mimetype='application/json')
+        @app.route('/uuid/<uuid>', methods=['GET'])
+        # return specific UUID
+        def fetch_uuid(uuid):
+            result = [i for i in self.soc_log if i['uuid']==uuid]
+            return flask.Response(json.dumps(result), mimetype='application/json')
             
         app.run(host='0.0.0.0', port=self.port)
         
